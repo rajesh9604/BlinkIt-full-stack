1effaa83da078dd61e7d7dec79aefa582d771d5e
rajesh9604 <rajeshgawade95.rg@gmail.com>
2025-07-25 16:27:51 +0530

    fix: updated  otpverificationPage.jsx to match filename

--- client/src/pages/OtpVerification.jsx
+++ client/src/pages/OtpVerification.jsx
@@ -5,7 +5,7 @@ import { FaRegEye } from "react-icons/fa6";
 import toast from "react-hot-toast";
 
 import Axios from "../utils/Axios";
-import SummaryApi from "../common/summaryApi";
+import SummaryApi from "../common/SummaryApi";
 import AxiosToastError from "../utils/AxiosToastError";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
